2e2665e5a86565fbceb40487fca887700abbd257
jamesl0350 <locklear.james.jl@gmail.com>
2025-02-22 13:07:26 -0500

    uncommented comments in server.js and added form to add a comment

--- routes/comment.js
+++ routes/comment.js
@@ -5,6 +5,6 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Post Routes - simplified for now
 
-router.post("/createComment", commentsController.createComment);
+router.post("/createComment/:id", commentsController.createComment);
 
 module.exports = router;
